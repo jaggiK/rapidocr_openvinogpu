c6be7991df429ed12c2ffc847fa27195a7f7d916
jaggik <jagadishkmahendran@gmail.com>
2023-05-05 18:06:55 -0400

    update release

--- setup.py
+++ setup.py
@@ -63,5 +63,5 @@ setuptools.setup(
     entry_points={
         'console_scripts': [f'{MODULE_NAME}={MODULE_NAME}.rapid_ocr_api:main'],
     },
-    # download_url="https://github.com/jaggiK/rapidocr_openvino_gpu/archive/refs/tags/1.0.5.tar.gz",
+    download_url="https://github.com/jaggiK/rapidocr_openvinogpu/archive/refs/tags/1.0.0.tar.gz",
 )
