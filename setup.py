8e8212df7b49dd76731602641f74444ce5a89c19
jaggik <jagadishkmahendran@gmail.com>
2023-05-07 20:51:56 -0400

    adding toml and cfg file

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
-# -*- encoding: utf-8 -*-
-# @Author: SWHL
-# @Contact: liekkaskono@163.com
 import sys
 from pathlib import Path
 
 import setuptools
-from get_pypi_latest_version import GetPyPiLatestVersion
 
 
 def get_readme():
@@ -19,49 +15,12 @@ def get_readme():
 
 MODULE_NAME = 'rapidocr_openvinogpu'
 
-obtainer = GetPyPiLatestVersion()
-latest_version = obtainer(MODULE_NAME)
-VERSION_NUM = "1.0.1"#obtainer.version_add_one(latest_version)
-
-# 优先提取commit message中的语义化版本号，如无，则自动加1
-if len(sys.argv) > 2:
-    match_str = ' '.join(sys.argv[2:])
-    matched_versions = obtainer.extract_version(match_str)
-    if matched_versions:
-        VERSION_NUM = matched_versions
-sys.argv = sys.argv[:2]
-
 setuptools.setup(
     name=MODULE_NAME,
-    version=VERSION_NUM,
     platforms="Any",
-    description="A cross platform OCR Library based on OpenVINO.",
-    long_description=get_readme(),
-    long_description_content_type='text/markdown',
-    author="SWHL",
-    author_email="jagadishkmahendran@gmail.com",
-    url="https://github.com/jaggiK/RapidOCR",
-    license='Apache-2.0',
     include_package_data=True,
-    install_requires=["pyclipper>=1.2.1", "openvino>=2022.2.0",
-                      "opencv_python>=4.5.1.48", "numpy>=1.19.3",
-                      "six>=1.15.0", "Shapely>=1.7.1", 'PyYAML', 'Pillow'],
-    package_dir={'': MODULE_NAME},
-    packages=setuptools.find_namespace_packages(where=MODULE_NAME),
-    package_data={'': ['*.onnx', '*.yaml', '*.txt']},
-    keywords=[
-        'ocr,text_detection,text_recognition,db,onnxruntime,paddleocr,openvino,rapidocr'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    python_requires='>=3.6,<=3.11',
-    entry_points={
-        'console_scripts': [f'{MODULE_NAME}={MODULE_NAME}.rapid_ocr_api:main'],
-    },
-    download_url="https://github.com/jaggiK/rapidocr_openvinogpu/archive/refs/tags/1.0.1.tar.gz",
+    # package_dir={'': MODULE_NAME},
+    packages=setuptools.find_packages(),
+    package_data={'': ['models/*.onnx', '*.yaml', '*.txt']},
+    python_requires='>=3.6,<=3.11'
 )
